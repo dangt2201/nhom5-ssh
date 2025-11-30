b1a19972be4bc6fc154ae4eef18b94b961226808
LuuNhatKhanh <luunhatkhanh2004vl@gmail.com>
2025-12-01 02:39:33 +0700

    Khoi tao du an web ban giay

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckAdmin;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Login\LoginController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BrandController;
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::middleware(['auth', CheckAdmin::class])->prefix('admin')->group(function () {
+
+    // Đường dẫn sẽ là: domain.com/admin/dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    // Đường dẫn quản lý sản phẩm: domain.com/admin/products/...
+    Route::resource('products', ProductController::class);
+
+});
+Route::get('/login', [LoginController::class, 'showLoginRegister'])->name('login');
+Route::post('/register', [LoginController::class, 'register'])->name('register');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/return-policy', function () {
+    return view('user.return_policy');
+})->name('return.policy');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/danh-muc/{slug}', [ProductController::class, 'getByCategory'])->name('shop.category');
+Route::get('/san-pham', [ProductController::class, 'index'])->name('shop.index');
+Route::get('/san-pham/{slug}.html', [ProductController::class, 'detail'])->name('shop.detail');
+// Route danh sách Hãng
+Route::get('/thuong-hieu', [BrandController::class, 'index'])->name('brands.index');
+Route::get('/thuong-hieu/{slug}', [BrandController::class, 'show'])->name('brands.show');
+Route::get('/hot-sale', [ProductController::class, 'hotSale'])->name('shop.hotSale');
